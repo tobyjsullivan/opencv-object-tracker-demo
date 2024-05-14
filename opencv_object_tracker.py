fc37e21e4ae4a6e8fac4a75dcd8448c4ff59aaee
Toby Sullivan <tobyjsullivan@gmail.com>
2024-05-14 15:01:04 -0700

    Track a found face automatically

--- opencv_object_tracker.py
+++ opencv_object_tracker.py
@@ -8,6 +8,7 @@ import time
 import cv2
 
 DEFAULT_TRACKER="kcf"
+FACE_CASCADE_FILE='data/haarcascades/haarcascade_frontalface_alt.xml'
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", type=str, help="path to input video file")
@@ -29,6 +30,12 @@ OPENCV_OBJECT_TRACKERS = {
 # grab the appropriate object tracker using our dictionary of
 # OpenCV object tracker objects
 tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+
+faces = []
+classifier = cv2.CascadeClassifier()
+if not classifier.load(cv2.samples.findFile(FACE_CASCADE_FILE)):
+ print('--(!)Error loading face cascade')
+ exit(0)
 
 # initialize the bounding box coordinates of the object we are going
 # to track
@@ -60,6 +67,22 @@ while True:
 	(H, W) = frame.shape[:2]
 
 	# check to see if we are currently tracking an object
+	if initBB is None:
+		# Peform face detection
+		# Source: https://docs.opencv.org/3.4/db/d28/tutorial_cascade_classifier.html
+
+		# Convert to greyscale
+		frame_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+		frame_gray = cv2.equalizeHist(frame_gray)
+		# Detect a face
+		faces = classifier.detectMultiScale(frame_gray)
+		if len(faces) > 0:
+			initBB = faces[0]
+			# start OpenCV object tracker using the supplied bounding box
+			# coordinates, then start the FPS throughput estimator as well
+			tracker.init(frame, initBB)
+			fps = FPS().start()
+
 	if initBB is not None:
 		# grab the new bounding box coordinates of the object
 		(success, box) = tracker.update(frame)
@@ -68,9 +91,11 @@ while True:
 			(x, y, w, h) = [int(v) for v in box]
 			cv2.rectangle(frame, (x, y), (x + w, y + h),
 				(0, 255, 0), 2)
+
 		# update the FPS counter
 		fps.update()
 		fps.stop()
+
 		# initialize the set of information we'll be displaying on
 		# the frame
 		info = [
@@ -83,6 +108,7 @@ while True:
 			text = "{}: {}".format(k, v)
 			cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
 				cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
 
 	# show the output frame
 	cv2.imshow("Frame", frame)
